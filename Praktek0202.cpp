caf3883c6ca4ac2080847277c642b11060d30cf5
Olga riswanti putri <32381022+olgaputri@users.noreply.github.com>
2017-10-20 23:05:37 +0700

    Membuat Variable dengan INISIALISASI nlainya

---
+++ Praktek0202.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int nilai=0;
+	int Nilai=1;
+	int NILAI=2;
+	int niLAi=3;
+	double angka(10.5),Angka,ANGKA(3.14);
+	int a(10),b(7),c(12);
+	cout<< a<<endl;
+	cout<< b<<endl;
+	cout<< c<<endl;
+	cout<< angka<<endl;
+	cout<< Angka<<endl;
+	cout<< ANGKA<<endl;
+}
