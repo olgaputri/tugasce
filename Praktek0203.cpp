c4d47c726d0a260b1da931c3116025d11a3114db
Olga riswanti putri <32381022+olgaputri@users.noreply.github.com>
2017-10-24 11:20:15 +0700

    konversi ounce ke ton

--- Praktek0203.cpp
+++ Praktek0203.cpp
@@ -5,4 +5,8 @@ int main(){
 	double berat;
 	int  ons,jumlah;
 	cout<<"Masukkan Berapa Berat Sereal Dalam 1 Kotak Kemasan (Dalam Satuan Ounce)= ";cin>>ons;
+	cout<<""<<endl;
+	berat=ons/ton;
+	jumlah=1/berat;
+	cout<<"1 Kotak kemasan sereal beratnya = "<<berat;cout<<" Ton"<<endl;
 }
