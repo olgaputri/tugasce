c9c0f584c782ccba9d3ed98c036a59002df7c11e
Olga riswanti putri <32381022+olgaputri@users.noreply.github.com>
2017-10-24 11:19:27 +0700

    Input Berat

--- Praktek0203.cpp
+++ Praktek0203.cpp
@@ -2,5 +2,7 @@
 using namespace std;
 int main(){
 	double ton=35273.92;
-
+	double berat;
+	int  ons,jumlah;
+	cout<<"Masukkan Berapa Berat Sereal Dalam 1 Kotak Kemasan (Dalam Satuan Ounce)= ";cin>>ons;
 }
