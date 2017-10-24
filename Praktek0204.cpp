f153851b15a8dfe95e1e3091b8b939cb6d30c9f7
Olga riswanti putri <32381022+olgaputri@users.noreply.github.com>
2017-10-24 11:32:38 +0700

    perbaikan(Luas 1 Box Parket)

--- Praktek0204.cpp
+++ Praktek0204.cpp
@@ -5,6 +5,7 @@ int main(){
 	double lantaiP = 193*1215;
 	double luas1Box = lantaiP*8/1000000;
 	cout << "Luas Rumah 		: "<<luasRumah<<" m2"<<endl;
+	cout << "Luas 1 box parket	: "<<luas1Box<<" m2"<<endl;
 
 }
 
