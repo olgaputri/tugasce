6f535276cd1cc6f5d41edfdb6e462c3c25744457
Olga riswanti putri <32381022+olgaputri@users.noreply.github.com>
2017-10-24 11:20:48 +0700

    output hasil konversi (berat ton)

--- Praktek0203.cpp
+++ Praktek0203.cpp
@@ -9,4 +9,6 @@ int main(){
 	berat=ons/ton;
 	jumlah=1/berat;
 	cout<<"1 Kotak kemasan sereal beratnya = "<<berat;cout<<" Ton"<<endl;
+	cout<<"Jika terdapat 1 Ton sereal, maka akan memerlukan = "<<jumlah;cout<<" Kotak"<<endl;
+	return 0;
 }
