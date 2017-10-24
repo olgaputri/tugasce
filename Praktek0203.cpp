d2c7db71091bc792994549323d59bb5b46e757a5
Olga riswanti putri <32381022+olgaputri@users.noreply.github.com>
2017-10-24 11:17:57 +0700

    inilisialisasi nilai Ton

--- Praktek0203.cpp
+++ Praktek0203.cpp
@@ -2,13 +2,5 @@
 using namespace std;
 int main(){
 	double ton=35273.92;
-	double berat;
-	int  ons,jumlah;
-	cout<<"Masukkan Berapa Berat Sereal Dalam 1 Kotak Kemasan (Dalam Satuan Ounce)= ";cin>>ons;
-	cout<<""<<endl;
-	berat=ons/ton;
-	jumlah=1/berat;
-	cout<<"1 Kotak kemasan sereal beratnya = "<<berat;cout<<" Ton"<<endl;
-	cout<<"Jika terdapat 1 Ton sereal, maka akan memerlukan = "<<jumlah;cout<<" Kotak"<<endl;
-	return 0;
+
 }
